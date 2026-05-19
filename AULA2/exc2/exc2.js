b9ee4dae9ff0515168d598a49ade4779e0861409
eduardodenzer-lab <lovadeus.eduardo@gmail.com>
2026-05-18 21:56:43 -0300

    Java18/05

---
+++ AULA2/exc2/exc2.js
@@ -0,0 +1,3 @@
+console.log('Hello World')
+console.log('Olá, meu nome é Eduardo Denzer')
+console.log('tenho 27 anos') 
---
+++ AULA2/exc2/exc4.js
@@ -0,0 +1,5 @@
+console.log('Lista de compras!') 
+console.log('- Arroz') 
+console.log('- Feijão') 
+console.log('- Carne') 
+console.log('- Frutas') 
