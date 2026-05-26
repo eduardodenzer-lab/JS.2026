15f7edb3567ddea28a902f18433225f56996a708
eduardodenzer-lab <lovadeus.eduardo@gmail.com>
2026-05-25 21:07:22 -0300

    Create aula2exc8.js

---
+++ .vscode/Aula 3/exemplos/aula2exc8.js
@@ -0,0 +1,5 @@
+// Dada uma variável idade com um número inteiro, crie uma variável idade_str que converta esse número para uma string. Em seguida, crie uma frase que diga "Eu tenho [idade] anos."
+let idade = 36;
+let idade_str = idade.toString(36);
+let frase = "Eu tenho " + idade_str + " anos.";
+console.log(frase);
