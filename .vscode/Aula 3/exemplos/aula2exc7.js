87b16b265921a9f442cc47cda32e5e16c6506f43
eduardodenzer-lab <lovadeus.eduardo@gmail.com>
2026-05-25 21:07:19 -0300

    Create aula2exc7.js

---
+++ .vscode/Aula 3/exemplos/aula2exc7.js
@@ -0,0 +1,8 @@
+let Nome = 'Fernando da Silva'
+let Cargo = 'Analista de Sistemas'
+let TempoEmpresa = 6 + ' anos'
+let Ativo = true
+console.log('Nome: ' + Nome)
+console.log('Cargo: ' + Cargo)
+console.log('Tempo na empresa: ' + TempoEmpresa)
+console.log('Ativo: ' + Ativo)
