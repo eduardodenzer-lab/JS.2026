0fcfe742d9c6151d12f9abcbd6bfdb7f85092ce3
eduardodenzer-lab <lovadeus.eduardo@gmail.com>
2026-05-25 21:58:23 -0300

    aula 2

--- .vscode/Aula 3/exemplos/aula2exc7.js
+++ .vscode/Aula 3/exemplos/aula2exc7.js
@@ -1,8 +1,22 @@
-let Nome = 'Fernando da Silva'
-let Cargo = 'Analista de Sistemas'
-let TempoEmpresa = 6 + ' anos'
-let Ativo = true
-console.log('Nome: ' + Nome)
-console.log('Cargo: ' + Cargo)
-console.log('Tempo na empresa: ' + TempoEmpresa)
-console.log('Ativo: ' + Ativo)
+let nome = 'Fernando da Silva'
+let cargo = 'Analista de Sistemas'
+let tempoEmpresa = 6 + ' anos'
+let ativo = true
+
+console.log(`Ficha de funcionario`)
+console.log(`Nome: ${nome}`)
+console.log(`Cargo: ${cargo}`)
+console.log(`Tempo de Empresa: ${tempoEmpresa}`)
+console.log(`Ativo: ${ativo}`)
+
+
+
+
+
+
+
+
+//console.log('Nome: ' + Nome)
+//console.log('Cargo: ' + Cargo)
+//console.log('Tempo na empresa: ' + TempoEmpresa)
+//console.log('Ativo: ' + Ativo)
--- .vscode/Aula 3/exemplos/aula2exc9.js
+++ .vscode/Aula 3/exemplos/aula2exc9.js
@@ -1,6 +1,7 @@
 //Dado o nome, sobrenome e idade de uma pessoa, usando concatenação de string, combine essas variáveis em uma única frase, que diga "Meu nome é [nome completo] e eu tenho [idade] anos."
 let nome = 'Eduardo';
 let sobrenome = 'Denzer';
+let nomeCompleto = nome + ' ' + sobrenome;
 let idade = 36;
-let frase = "Meu nome é " + nome + " " + sobrenome + " e eu tenho " + idade + " anos.";
+let frase = "Meu nome é " + nomeCompleto + " e eu tenho " + idade + " anos.";
 console.log(frase);
