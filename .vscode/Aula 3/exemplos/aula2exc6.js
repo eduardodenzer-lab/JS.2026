6a5235e7c5456c2359e9e69eb5711ff94c088caa
eduardodenzer-lab <lovadeus.eduardo@gmail.com>
2026-05-25 21:07:14 -0300

    Create aula2exc6.js

---
+++ .vscode/Aula 3/exemplos/aula2exc6.js
@@ -0,0 +1,9 @@
+let Titulo = 'Homem-Aranha: Sem Volta Para Casa';
+let Ano = 2021;
+let Duracao = 148;
+let clasificacao = 'Livre'
+
+console.log('Título: ' + Titulo);
+console.log('Ano: ' + Ano);
+console.log('Duração: ' + Duracao + ' minutos');
+console.log('Classificação: ' + clasificacao);
