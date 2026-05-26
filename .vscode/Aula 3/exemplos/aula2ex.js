b1ed9c5b538d2d6727a804028bdd0f555cbbb791
eduardodenzer-lab <lovadeus.eduardo@gmail.com>
2026-05-25 21:58:09 -0300

    Create aula2ex.js

---
+++ .vscode/Aula 3/exemplos/aula2ex.js
@@ -0,0 +1,6 @@
+let nome = 'Eduardo';
+let sobrenome = 'Denzer'
+let nomeCompleto = nome + ' ' + sobrenome
+let idade = 36
+
+console.log(`Meu nome é ${nome} ${sobrenome}, e eu tenho, ${idade}, anos`)
