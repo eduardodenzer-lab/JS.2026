a9327f61e5e58df7e0c52293560ca845bac78caf
eduardodenzer-lab <lovadeus.eduardo@gmail.com>
2026-05-25 21:07:29 -0300

    Create aula2exc9.js

---
+++ .vscode/Aula 3/exemplos/aula2exc9.js
@@ -0,0 +1,6 @@
+//Dado o nome, sobrenome e idade de uma pessoa, usando concatenação de string, combine essas variáveis em uma única frase, que diga "Meu nome é [nome completo] e eu tenho [idade] anos."
+let nome = 'Eduardo';
+let sobrenome = 'Denzer';
+let idade = 36;
+let frase = "Meu nome é " + nome + " " + sobrenome + " e eu tenho " + idade + " anos.";
+console.log(frase);
